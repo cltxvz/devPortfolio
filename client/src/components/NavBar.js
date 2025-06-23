6bcee7c584e50ad33474c4ee8b8a537a4c3167fe
cltxvz <carlos.cardenas09@outlook.com>
2025-06-23 09:04:36 -0400

    Added states to NavBar for better user experience

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,23 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function CustomNavbar() {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top" expanded={expanded}>
       <Container>
-        <Navbar.Brand as={Link} to="/">
-        Carlos A. Cárdenas
+        <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>
+          Carlos A. Cárdenas
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="portfolio-navbar" />
+        <Navbar.Toggle aria-controls="portfolio-navbar" onClick={() => setExpanded(expanded ? false : "expanded")} />
         <Navbar.Collapse id="portfolio-navbar">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/about" onClick={() => setExpanded(false)}>About</Nav.Link>
+            <Nav.Link as={Link} to="/projects" onClick={() => setExpanded(false)}>Projects</Nav.Link>
+            <Nav.Link as={Link} to="/contact" onClick={() => setExpanded(false)}>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
